a48763a56a4763f55fbf1f05027fdb0c277dab64
hawa-bah <56238080+hawa-bah@users.noreply.github.com>
2020-12-03 00:42:42 +0000

    paggination query done, I commented the search query

--- mongo-assesment/server.js
+++ mongo-assesment/server.js
@@ -12,6 +12,7 @@ const port = 4000;
 
 // << db setup >>
 const db = require("./db");
+const { query } = require("express");
 const dbName = "movie";
 const collectionName = "film";
 
@@ -24,12 +25,63 @@ db.initialize(
     // successCallback
     // get all items
     dbCollection.find().toArray(function (err, result) {
-      if (err) throw err;
-      console.log(result);
+      if (err) {
+        throw err;
+      }
+      if (err) {
+        console.log("THERE IS AN ERROR");
+      }
+      // console.log(result);
     });
 
     // << db CRUD routes >>
+    //part 3 homework
+    server.get("/api/film/:pId", (request, response) => {
+      const pageId = request.params.pId;
+      const numItem = 10;
+      // let query = {};
+      // query.skip = (pageId - 1) * 10;
+      // query.limit = numItem;
+
+      dbCollection
+        .find({})
+        .skip((pageId - 1) * 10)
+        .limit(numItem)
+        .sort({ title: -1 })
+        .toArray((error, result) => {
+          if (error) throw error;
+          // if (pageId === 0) {
+          //   console.log("WTF");
+          //   result = {
+          //     message: "The pagination numer is incorrect",
+          //   };
+          //   response.json(result);
+          // } else {
+          // return item
+          response.json(result);
+          // console.log(reults);
+          // }
+        });
+    });
   },
+  // part 5 homework------ start work here
+  // server.get(`/film/title?keyword=`, (request, response) => {
+  //   const pageId = request.params.pId;
+  //   const numItem = 10;
+  //   // let query = {};
+  //   // query.skip = (pageId - 1) * 10;
+  //   // query.limit = numItem;
+
+  //   dbCollection
+  //     .find({ $text: { $search: keyword } })
+  //     .toArray((error, result) => {
+  //       if (error) throw error;
+  //       response.json(result);
+  //       // console.log(reults);
+  //       // }
+  //     });
+  // }),
+
   function (err) {
     // failureCallback
     throw err;
